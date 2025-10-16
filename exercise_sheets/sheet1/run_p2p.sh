2ce20742510d3985d4de082dd2562fb545e107cd
Alex Mecklin <alex.mecklin@hotmail.com>
2025-10-16 23:05:00 +0300

    old sheet 1 script commited

--- exercise_sheets/sheet1/run_p2p.sh
+++ exercise_sheets/sheet1/run_p2p.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -l
 #SBATCH --account=project_462001048
 #SBATCH --partition=debug
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=2
+#SBATCH --nodes=2
+#SBATCH --ntasks-per-node=1
 ##SBATCH --output=p2p.out
 
 srun ./p2p_program
