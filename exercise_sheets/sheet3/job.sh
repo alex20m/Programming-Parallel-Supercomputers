244a1a462feefd509c3bdc9f2f5c7482f4b5e0dc
Alex Mecklin <alex.mecklin@hotmail.com>
2025-10-16 14:09:51 +0300

    scorep added

--- exercise_sheets/sheet3/job.sh
+++ exercise_sheets/sheet3/job.sh
@@ -19,7 +19,19 @@
 ##SBATCH --output=DE.out
 ####Special resource allocation, do not use unless instructed
 
+export EBU_USER_PREFIX=/projappl/project_462001048/EB/
+export SCOREP_EXPERIMENT_DIRECTORY=scorep_experiment_${SLURM_JOBID}
+
+ml LUMI/24.03
+ml partition/C
+ml Score-P/9.0-cpeGNU-24.03
+ml Scalasca/2.6.2-cpeGNU-24.03
+
+export SCOREP_TOTAL_MEMORY=100M
+export SCAN_TRACE_FILESYS=${PWD}${SCAN_TRACE_FILESYS:+:${SCAN_TRACE_FILESYS}}
+
+
 rm -f HEAT_RESTART.dat
 export SRUN_CPUS_PER_TASK=$SLURM_CPUS_PER_TASK
-time srun heat_mpi 4000 4000 1000
+time scalasca -analyze srun heat_mpi 4000 4000 1000
 
